ea295597980100137f3f85e1564f9fd925e92cd8
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2023-07-23 08:57:12 +0530

    Create fullest_row.m

---
+++ fullest_row.m
@@ -0,0 +1,15 @@
+function f = fullest_row(n)
+    i = 1;
+    max = 0;
+    row_idx = 0;
+    [l, w] = size(n);
+    while i <= l
+        temp = sum(n(i,:) ~= 0);
+        if temp > max
+            max = temp;
+            row_idx = i;    
+        end 
+        i = i + 1;
+    end
+    disp(row_idx);
+end
